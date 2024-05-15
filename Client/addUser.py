6da563eadaf857fa9dc9bba9bf6c915d73a1e9c8
St0522 <steven010522@gmail.com>
2024-05-15 15:17:04 +0900

    new file:   Client/__pycache__/addBook.cpython-39.pyc 	new file:   Client/__pycache__/addUser.cpython-39.pyc 	new file:   Client/__pycache__/admin.cpython-39.pyc 	modified:   Client/__pycache__/bookList.cpython-39.pyc 	modified:   Client/addUser.py

--- Client/addUser.py
+++ Client/addUser.py
@@ -16,12 +16,15 @@ class AddUserWindow(QWidget):
         self.lineEdit_user_name.setPlaceholderText('회원 이름')
         self.lineEdit_user_id = QLineEdit()
         self.lineEdit_user_id.setPlaceholderText('학번')
+        self.lineEdit_user_passwd = QLineEdit()
+        self.lineEdit_user_passwd.setPlaceholderText('비밀 번호')
 
         self.add_button = QPushButton('추가')
         self.add_button.clicked.connect(self.addUser)
 
         layout.addWidget(self.lineEdit_user_name)
         layout.addWidget(self.lineEdit_user_id)
+        layout.addWidget(self.lineEdit_user_passwd)
         layout.addWidget(self.add_button)
 
         self.setLayout(layout)
@@ -29,9 +32,10 @@ class AddUserWindow(QWidget):
     def addUser(self):
         user_name = self.lineEdit_user_name.text()
         user_id = self.lineEdit_user_id.text()
+        user_passwd= self.lineEdit_user_passwd.text()
 
-        if not user_name or not user_id:
-            QMessageBox.warning(self, '입력 오류', '회원 이름과 학번을 모두 입력해야 합니다.')
+        if not user_name or not user_id or not user_passwd:
+            QMessageBox.warning(self, '입력 오류', '회원 이름과 학번을, 비번을 모두 입력해야 합니다.')
         else:
             QMessageBox.information(self, '성공', '회원이 추가되었습니다.')
             self.close()
